3d7f5c57f8b16dbffc6a94a8ee2e0f62fad3e2eb
DESKTOP-5K1Q4JV\Dachi <kiu_skhiladze.dachi>
2022-04-11 12:17:16 +0400

    initial commit

---
+++ Builder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace Builder
+{
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+            var hello = "hello";
+            var sb = new StringBuilder();
+            sb.Append("<p>");
+            sb.AppendLine(hello);
+            sb.Append("</p>");
+            Console.WriteLine(sb);
+
+            var words = new[] { "hello", "world" };
+            sb.Clear();
+            sb.Append("<ul>");
+            foreach (var word in words)
+            {
+                sb.AppendFormat("<li>{0}</li>", word);
+            }
+            sb.Append("</ul>");
+            Console.WriteLine(sb);
+
+            var builder = new HtmlBuilder("ul");
+            builder.AddChild("li", "hello").AddChild("li", "hello");
+            Console.WriteLine(builder.ToString());
+        }
+    }
+}
