d8ef687e7e9a9a220ea28a55a7566b65cb83c7eb
DESKTOP-5K1Q4JV\Dachi <kiu_skhiladze.dachi>
2022-04-11 12:32:48 +0400

    Added my project

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -6,23 +6,6 @@ namespace Builder
     {
         static void Main(string[] args)
         {
-            var hello = "hello";
-            var sb = new StringBuilder();
-            sb.Append("<p>");
-            sb.AppendLine(hello);
-            sb.Append("</p>");
-            Console.WriteLine(sb);
-
-            var words = new[] { "hello", "world" };
-            sb.Clear();
-            sb.Append("<ul>");
-            foreach (var word in words)
-            {
-                sb.AppendFormat("<li>{0}</li>", word);
-            }
-            sb.Append("</ul>");
-            Console.WriteLine(sb);
-
             var builder = new HtmlBuilder("ul");
             builder.AddChild("li", "hello").AddChild("li", "hello");
             Console.WriteLine(builder.ToString());
